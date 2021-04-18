val-verde/indent

commit c89d32a14642a80daf4d68f44cacee99c05455b6

Move need_chars into handletoken.c

src/handletoken.c
@@ -76,9 +76,29 @@
 RCSTAG_CC ("$GNU$");
 
 /**
+ * Expand a buffer to hold more chars, aligned on a
+ * 1 KB boundary.
+ */
+extern void need_chars(buf_ty * bp, size_t needed)
+{
+    size_t current_size = (size_t)(bp->end - bp->ptr);
+
+    if (current_size + needed >= bp->size)
+    {
+        bp->size = ((current_size + needed) & (size_t)~1023);
+        bp->ptr = xrealloc(bp->ptr, bp->size);
+        if (bp->ptr == NULL)
+        {
+            fatal (_("Ran out of memory"), 0);
+        }
+
+        bp->end = bp->ptr + current_size;
+    }
+}
+
+/**
  *
  */
-
 extern void check_code_size(void)
 {
     if (e_code >= l_code)                               

src/indent.h
@@ -66,14 +66,6 @@ RCSTAG_H (indent, "$Id$");
 
 #include "lexi.h"
 
-/**
- * Round up P to be a multiple of SIZE.
- */
-
-#ifndef ROUND_UP
-#define ROUND_UP(p, size) (((unsigned long) (p) + (size) - 1) & ~((size) - 1))
-#endif
-
 /** Values that `indent' can return for exit status.
  *
  *  `total_success' means no errors or warnings were found during a successful

src/handletoken.h
@@ -20,28 +20,5 @@ extern void handle_the_token(
     BOOLEAN        * pbreak_line);
 
 extern void check_code_size(void);
-
-/**
- *
- */
-
-static void INLINE need_chars (
-    buf_ty * bp,
-    int      needed)
-{
-    int current_size = (bp->end - bp->ptr);
-
-    if ((current_size + needed) >= bp->size)
-    {
-        bp->size = ROUND_UP (current_size + needed, 1024);
-        bp->ptr = xrealloc (bp->ptr, bp->size);
-        if (bp->ptr == NULL)
-        {
-            fatal (_("Ran out of memory"), 0);
-        }
-
-        bp->end = bp->ptr + current_size;
-    }
-}
-
+extern void need_chars(buf_ty * bp, size_t needed);
 #endif

commit 02d7fd4c426e4acfa591a6738dec72f7303c1e7e

Restore the ROUND_UP macro and adjust the initial buffer size.
When need_chars was moved from "handletoken.h" to "handletoken.c",
the ROUND_UP macro was removed, but the replacement was incorrect.

This caused the program to exit with a "Virtual memory exhausted"
error when it tried to reallocate 0 bytes (thus freeing the memory).
It reallocated to 0 bytes because the initial buffer size was less
than 1024, and the size calculation rounds down instead of up.

Bug: #56644
Fixes: c89d32a

src/handletoken.c
@@ -85,7 +85,7 @@ extern void need_chars(buf_ty * bp, size_t needed)
 
     if (current_size + needed >= (size_t)bp->size)
     {
-        bp->size = ((current_size + needed) & (size_t)~1023);
+        bp->size = ROUND_UP (current_size + needed, 1024);
         bp->ptr = xrealloc(bp->ptr, bp->size);
         if (bp->ptr == NULL)
         {

src/indent.h
@@ -66,6 +66,14 @@ RCSTAG_H (indent, "$Id$");
 
 #include "lexi.h"
 
+/**
+ * Round up P to be a multiple of SIZE.
+ */
+
+#ifndef ROUND_UP
+#define ROUND_UP(p, size) (((unsigned long) (p) + (size) - 1) & ~((size) - 1))
+#endif
+
 /** Values that `indent' can return for exit status.
  *
  *  `total_success' means no errors or warnings were found during a successful

src/parse.c
@@ -53,7 +53,7 @@ RCSTAG_CC ("$Id$");
 
 parser_state_ty *parser_state_tos = NULL;
 
-#define INITIAL_BUFFER_SIZE 1000
+#define INITIAL_BUFFER_SIZE 1024
 #define INITIAL_STACK_SIZE 2
 
 /**